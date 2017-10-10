9c3763143062f3fc58612773d0e2cd6fe5436af4
Meruem <martin.kuben@seznam.cz>
2017-10-10 17:22:17 +0200

    Update Actions.fs

--- Rokugan/Actions.fs
+++ Rokugan/Actions.fs
@@ -5,22 +5,39 @@ open GameTypes
 open PlayerState
 open GameState
 open PlayerActions
+open CardRepository
 
 let getAllPlayableActions player gs  =
+    let ps = gs |> playerState player 
+    let actions =
+        [for card in ps.CardsInPlayList do
+            let cardDef = repository.GetCard card.Title
+            for action in CardDef.actions cardDef do
+                if action.Condition gs then yield action]
+    
+
     [{ Type =  PlayerActionType.Test
        Player = player
-       OnExecute = none}]
+       OnExecute = none ()}]
 
-// let actionWindow gs =
-//     if hasPassed gs.Player1State && hasPassed gs.Player2State then none
-//     else 
-//         let passAction = 
-//             pass 
-//                 gs.ActivePlayer 
-//                 (changes 
-//                     [ ActionPass gs.ActivePlayer
-//                       SwitchActivePlayer]
-//                  >+> actionWindow      ) 
+let rec actions gs =
+    if hasPassed gs.Player1State && hasPassed gs.Player2State then []
+    else
+        let passAction = 
+            pass 
+                gs.ActivePlayer 
+                (changes 
+                    [ ActionPass gs.ActivePlayer
+                      SwitchActivePlayer]
+                 >+> playerActions actions) 
+        passAction :: (getAllPlayableActions gs.ActivePlayer gs)
+    
+
+let actionWindow gs =
+    changes [CleanPassFlags]
+    >+> playerActions actions
 
 let onActionPass player gs =
     gs |> changePlayerState player passPlayer
+
+let onCleanPassFlags gs = gs |> cleanPass
