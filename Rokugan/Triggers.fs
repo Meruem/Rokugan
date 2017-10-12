9cca1e9fcd7bbcef1e1b56f0c85c7d9d71cf6487
Meruem <martin.kuben@seznam.cz>
2017-10-12 18:05:41 +0200

    Update Triggers.fs

--- Rokugan/Triggers.fs
+++ Rokugan/Triggers.fs
@@ -2,11 +2,11 @@ module Triggers
 
 open RokuganShared
 open GameTypes
-let addTrigger trigger gs =
-   {gs with Triggers = trigger :: gs.Triggers} 
+let addTrigger (trigger: GameTrigger<'a,'b,'c>) (gm:GameModel<'a,'b,'c>) =
+   {gm with Triggers = trigger :: gm.Triggers} 
 
-let removeTrigger triggerName gs =
-    { gs with Triggers = gs.Triggers |> List.filter (fun t -> t.Name <> triggerName)}  
+let removeTrigger triggerName (gm:GameModel<'a,'b,'c>) =
+    { gm with Triggers = gm.Triggers |> List.filter (fun t -> t.Name <> triggerName)}  
 
 // let applyTriggers gs = 
 //     let triggers = gs.Triggers |> List.filter (fun t -> t.Condition gs)
@@ -18,7 +18,7 @@ let removeTrigger triggerName gs =
 // let cleanPhaseTriggers gs = 
 //     { gs with Triggers = gs.Triggers |> List.filter (fun t -> t.Lifetime <> Phase)}    
 
-let addWinConditionsTriggers gs =
+let addWinConditionsTriggers (gs:GameModel<GameState,Command,'c>) =
     let pl1NoHonor cmd gs = gs.Player1State.Honor <= 0
     let pl2NoHonor cmd gs = gs.Player2State.Honor <= 0
     let pl1over25honor cmd gs = gs.Player1State.Honor >= 25
@@ -28,7 +28,7 @@ let addWinConditionsTriggers gs =
     let pl2Win = changes [EndGame Player2Won] // {gs with GamePhase = End Player2Won; Actions = [] }
     let pl1Win = changes [EndGame Player1Won] // {gs with GamePhase = End Player1Won; Actions = [] }
     let triggers = [
-      { Name = "Player1 military victory"
+      { GameTrigger.Name = "Player1 military victory"
         Lifetime = Once
         Condition = pl2BrokenStronghold
         Transform = pl1Win }
@@ -54,5 +54,24 @@ let addWinConditionsTriggers gs =
         Transform = pl2Win }]
     { gs with Triggers = List.append triggers gs.Triggers}
 
+let gameTrigger name lifetime condition transform =
+  { GameTrigger.Name = name
+    Lifetime = lifetime 
+    Condition = condition
+    Transform = transform }
 
+let addCardTriggers (card:Card) (gm:GameModel<GameState, Command, PlayerActionType>) = 
+    let cardDef = CardRepository.repository.GetCard card.Title
+    let newTriggers =
+        cardDef.Triggers 
+        |> List.map (fun ct ->
+            gameTrigger 
+                (sprintf "%A[%d]: %s" card.Title card.Id ct.Name)
+                ct.Lifetime
+                (ct.Condition card)
+                (ct.Transform card))
+    {gm with Triggers = newTriggers @ gm.Triggers}
 
+let addAllCardsTriggers (gm:GameModel<GameState, Command, PlayerActionType>) = 
+    gm.State.Cards 
+    |> List.fold (fun acc card -> addCardTriggers card acc) gm
