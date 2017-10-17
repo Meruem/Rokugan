fc64619a350369f65c505a95207627586f977050
Meruem <martin.kuben@seznam.cz>
2017-10-17 18:38:12 +0200

    Update TestCards.fs

--- Rokugan/CardSets/TestCards.fs
+++ Rokugan/CardSets/TestCards.fs
@@ -3,31 +3,32 @@ module TestCards
 open RokuganShared
 open GameTypes
 open CardDef
+open CardDef.CardDef
 
 let ``Generic 1/1`` = 
     title "Generic 1/1" 
-    @+> dynastychar 1 (Some 1) (Some 1) 0 []
+    @+ dynastychar 1 (Some 1) (Some 1) 0 []
 
 let ``Generic 2/2`` =
     title "Generic 2/2" 
-    @+> dynastychar 2 (Some 2) (Some 2) 0 []
+    @+ dynastychar 2 (Some 2) (Some 2) 0 []
         
 let ``Holding`` =
     title  "Holding" 
-    @+> holding 1 []
+    @+ holding 1 []
 
 let ``Event`` =
     title "Event"
-    @+> event 1
+    @+ event 1
 let ``Attachment`` =
     title "Attachment"
-    @+> attachment 1 2 0 [Weapon]
+    @+ attachment 1 2 0 [Weapon]
 let ``Province str 4``=
     title "Province str 4"
-    @+> province 4 Earth
+    @+ province 4 Earth
 let ``Stronghold`` =
     title "Stronghold"
-    @+> stronghold 0 10 7 10
+    @+ stronghold 0 10 7 10
 let testCards = 
   [ ``Generic 1/1``
     ``Generic 2/2``
